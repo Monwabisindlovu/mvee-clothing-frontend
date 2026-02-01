0efc5fcc74cafc1dbe673e09e1a786cf33947f07
monwabisindlovu <monwabisindlovu78@gmail.com>
2026-02-01 21:16:35 +0200

    Rename ui_temp back to ui and track files

--- src/components/ui_temp/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './button';
-export * from './card';
-export * from './checkbox';
-export * from './input';
-export * from './select';
-export * from './sheet';
-export * from './slider';
