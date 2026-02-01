80bd3c4d762d00c05123fb5fb4dbe7c6d6cf2628
monwabisindlovu <monwabisindlovu78@gmail.com>
2026-02-01 13:40:57 +0200

    fixing vercel error  logs

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from './button';
+export * from './card';
+export * from './checkbox';
+export * from './input';
+export * from './select';
+export * from './sheet';
+export * from './slider';
