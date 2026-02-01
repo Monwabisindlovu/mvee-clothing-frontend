8e53d5e0ab32502330c9cdc651fdc32efa42e0eb
monwabisindlovu <monwabisindlovu78@gmail.com>
2026-02-01 17:06:31 +0200

    fixing ts-config file'

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const path = require('path');
+// next.config.mjs
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -26,4 +32,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
